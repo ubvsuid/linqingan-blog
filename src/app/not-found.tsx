50b481e7fd42b044bb520480c26af3cbb7d3a93f
linqingan <linqingan501@gmail.com>
2026-07-17 23:25:50 +0800

    Offer search from the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,10 +8,15 @@ export default function NotFoundPage() {
       <Container>
         <p className="eyebrow">ERROR 404</p>
         <h1>这个页面不存在</h1>
-        <p>它可能已经移动、被删除，或者网址输入有误。</p>
-        <Link href="/" className="button button-primary">
-          返回首页
-        </Link>
+        <p>它可能已经移动、被删除，或者网址输入有误。可以返回首页，也可以直接搜索文章、术语和错误码。</p>
+        <div className="button-row">
+          <Link href="/" className="button button-primary">
+            返回首页
+          </Link>
+          <Link href="/search" className="button button-secondary">
+            搜索网站
+          </Link>
+        </div>
       </Container>
     </main>
   );
