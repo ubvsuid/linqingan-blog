76e35733cd5a7bf877601503eb7a8e529342df44
linqingan <linqingan501@gmail.com>
2026-07-24 16:15:03 +0800

    Render body article verification status correctly

--- src/app/en/blog/screeps-creep-body-parts/page.tsx
+++ src/app/en/blog/screeps-creep-body-parts/page.tsx
@@ -124,7 +124,7 @@ export default function EnglishCreepBodyPartsPage() {
       publishedLabel={article.publishedLabel}
       readingTime={article.readingTime}
       tags={article.tags}
-      verification={article.verification}
+      verification={article.verification.map(([term, value]) => ({ term, value }))}
       toc={article.toc}
       articleHtml={article.articleHtml}
       jsonLd={jsonLd}
