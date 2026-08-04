733ab8a40173b8ec42f9585d6de04bb430a1b6df
linqingan <linqingan501@gmail.com>
2026-08-04 17:20:10 +0800

    feat(english): register access recycle destroy overrides

---
+++ src/lib/english-editorial-access-recycle-destroy-20260804.ts
@@ -0,0 +1,12 @@
+import { englishEditorialRampartAccessArticle20260804 } from "./english-editorial-rampart-access-20260804";
+import { englishEditorialRecycleArticle20260804 } from "./english-editorial-recycle-20260804";
+import { englishEditorialStructureDestroyArticle20260804 } from "./english-editorial-structure-destroy-20260804";
+
+export const englishEditorialAccessRecycleDestroyOverrides20260804 = {
+  [englishEditorialRampartAccessArticle20260804.slug]:
+    englishEditorialRampartAccessArticle20260804,
+  [englishEditorialRecycleArticle20260804.slug]:
+    englishEditorialRecycleArticle20260804,
+  [englishEditorialStructureDestroyArticle20260804.slug]:
+    englishEditorialStructureDestroyArticle20260804,
+};
